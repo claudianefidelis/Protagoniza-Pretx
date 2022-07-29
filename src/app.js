1b1ee7c947b931980ebe4dd7caad1c49d9b9565b
Claudiane Fidelis <claudianeclauu@gmail.com>
2022-07-29 19:34:18 -0300

    modificações index

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv-safe').config()
 const express = require('express')
 const cors = require('cors')
 const projetoRoutes = require('./routes/projetosRoutes')
@@ -7,6 +7,7 @@ const mongoose = require('./database/mongooseConnect')
 const projetosRoutesModel = require('./routes/projetosRoutesModel')
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger/swagger_output.json');
+const index = require('./routes/index')
 
 
 app.use(express.json())
@@ -14,7 +15,9 @@ app.use(express.json())
 app.use(cors())
 app.use('/projetos', projetoRoutes)
 app.use(projetosRoutesModel)
+app.use(index)
 app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 
 mongoose.connect()
 
--- src/controller/projetosController.js
+++ src/controller/projetosController.js
@@ -1,8 +1,6 @@
 const projetos = require('../models/projetos')
 const express = require('express')
-const app = express()
 
-app.use(express.json())
 
 
 
