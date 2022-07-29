c4c8ce3b2c1993f5921e89202b82f7da70ab4b14
Claudiane Fidelis <claudianeclauu@gmail.com>
2022-07-29 18:18:26 -0300

    instalação e config swagger

--- src/app.js
+++ src/app.js
@@ -5,11 +5,16 @@ const projetoRoutes = require('./routes/projetosRoutes')
 const app = express()
 const mongoose = require('./database/mongooseConnect')
 const projetosRoutesModel = require('./routes/projetosRoutesModel')
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger/swagger_output.json');
+
 
 app.use(express.json())
+
 app.use(cors())
 app.use('/projetos', projetoRoutes)
 app.use(projetosRoutesModel)
+app.use('/my-documentation-route', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 mongoose.connect()
 
