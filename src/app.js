c68ffc07c7b368915aefdfbcbf1a504db5cf9a9d
Claudiane Fidelis <claudianeclauu@gmail.com>
2022-07-30 00:56:34 -0300

    correção app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const projetoRoutes = require('./routes/projetosRoutes')
+const projetoRoutes = require('./routes/projetosRoutes.js')
 const app = express()
 const mongoose = require('./database/mongooseConnect.js')
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger/swagger_output.json');
-const index = require('./routes/index')
+const index = require('./routes/index.js')
 
 
 app.use(express.json())
